f9c7021c9ef8e9a4a3aa610c9296e1fda6037899
Sanjok <koolse@outlook.com>
2015-04-30 20:37:39 +0300

    Initial commit

---
+++ Events/Controllers/MainController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Events.Controllers
+{
+    public class MainController : Controller
+    {
+        //
+        // GET: /Main/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        public ActionResult List()
+        {
+            Events.Models.EventData model = new Events.Models.EventData();
+            return View(model);
+        }
+
+        public ActionResult Event(int id)
+        {
+            ViewBag.id = id;
+            return View();
+        }
+
+    }
+}
---
+++ Events/Models/Events.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections;
+
+namespace Events.Models
+{
+    public class Event
+    {
+        public Event(int id, string name, string desc, string location)
+        {
+            Id = id;
+            Name = name;
+            Desc = desc;
+            Location = location;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Desc { get; set; }
+        public string Location { get; set; }
+    }
+    public class EventData
+    {
+        private static List<Event> list;
+
+        public List<Event> getEvents()
+        {
+            list = new List<Event>();
+            for (int i=0; i<10;i++){
+                list.Add(new Event(i, "Name"+i, "Desc"+i, "Location"+i));
+            }
+            return list;
+        }
+
+        public static Event getEvent(int id)
+        {
+            return list[id];
+        }
+    }
+}
